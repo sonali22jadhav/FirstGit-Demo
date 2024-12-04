317d0a53ab1164a411658208674b59581ade4f11
Sonali Jadhav <sonali.jadhav@tatanexarc.com>
2024-12-04 17:15:00 +0530

    First commit

---
+++ src/Basic/Factors.java
@@ -0,0 +1,18 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class Factors {
+	public static void main(String args[])
+	{
+		int fact = 1;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int x=sc.nextInt();
+		for(int i=x; i>0;i--)
+		{
+		  fact=fact*i;
+		}
+		System.out.println("Factorial of number: "+fact);
+	}
+}
